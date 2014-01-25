56343b6c74831ecd494c83a29f44e836af2211bf
Programming <Programming@10.30.107.10>
2014-01-25 11:43:56 -0600

    Created the base platform with a 1 joystick drive system.

--- src/edu/wpi/first/wpilibj/paradigm/DriveTrain.java
+++ src/edu/wpi/first/wpilibj/paradigm/DriveTrain.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj.Talon;
  */
 public class DriveTrain {
 
-    
     DriverControls operatorInputs;
 
     final int LEFT_PORT = 1;
@@ -30,40 +29,41 @@ public class DriveTrain {
 
     double speedMult = 1;
     double fixNum;
-    
-    public DriveTrain(DriverControls _operatorInputs){
+
+    public DriveTrain(DriverControls _operatorInputs) {
         this.operatorInputs = _operatorInputs;
     }
+
     public double fix(double v) {
-        if (v > 1.0) {
-            return ((v - 1.0) * speedMult);
-        } else if (v < -1.0) {
-            return ((v + 1.0) * speedMult);
-        }
-        return v;
+//        if (v > 1.0) {
+//            return ((v - 1.0) * speedMult);
+//        } else if (v < -1.0) {
+//            return ((v + 1.0) * speedMult);
+//        }
+        return v / fixNum;
     }
 
     public double LeftMotor() {
-        return leftPow + fix(rightPow);
+        return fix(leftPow);
     }
 
     public double RightMotor() {
-        return rightPow + fix(leftPow);
+        return fix(rightPow);
     }
     //fix(),RightMotor(),and LeftMotor() are all used for the tank drive algorithm to correct the value differences of the axis
-   
+
     public void setPower() {
         joyStickX = operatorInputs.joystickX();
         joyStickY = operatorInputs.joystickY();
-        if (joyStickX==0 || joyStickY==0){
+        if (joyStickX == 0 || joyStickY == 0) {
             fixNum = 1;
-        } else{
-            if (Math.abs(joyStickX)>Math.abs(joyStickY)){
-                double fixNumMult = 1/Math.abs(joyStickX);
-                fixNum = Math.abs(joyStickY)*fixNumMult + 1; //1 = math.abs(joyStickX)*fixnum 1
+        } else {
+            if (Math.abs(joyStickX) > Math.abs(joyStickY)) {
+                double fixNumMult = 1 / Math.abs(joyStickX);
+                fixNum = Math.abs(joyStickY) * fixNumMult + 1; //1 = math.abs(joyStickX)*fixnum 1
             } else {
-                 double fixNumMult = 1/Math.abs(joyStickY);
-                fixNum = Math.abs(joyStickX)*fixNumMult + 1; //1 = math.abs(joyStickY)*fixnum 1
+                double fixNumMult = 1 / Math.abs(joyStickY);
+                fixNum = Math.abs(joyStickX) * fixNumMult + 1; //1 = math.abs(joyStickY)*fixnum 1
             }
         }
         leftPow = -joyStickY - joyStickX;
