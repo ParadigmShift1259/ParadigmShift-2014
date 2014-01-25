1bc9cd98c37a122f4d81fdb03943caf057de0b74
Programming <Programming@10.30.107.10>
2014-01-25 11:43:46 -0600

    Created the base platform with a 1 joystick drive system.

--- src/edu/wpi/first/wpilibj/paradigm/DriverControls.java
+++ src/edu/wpi/first/wpilibj/paradigm/DriverControls.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.wpi.first.wpilibj.paradigm;
+
 import edu.wpi.first.wpilibj.Joystick;
 
 /**
@@ -12,27 +12,29 @@ import edu.wpi.first.wpilibj.Joystick;
  * @author Programming
  */
 public class DriverControls {
-    
+
     private Joystick joystick = new Joystick(1); //Created and initialized the joystick controller
     private Joystick xBox = new Joystick(2); //Created and initialized the xbox controller
-    
-    
-    public double joystickX(){
-        return this.joystick.getX(); //return the value of the x-axis of the joystick controller
+    final double DEADZONE = .04;
+
+    public double joystickX() {
+        return deadzoneFilter(this.joystick.getX()); //return the value of the x-axis of the joystick controller
     }
-    
-    public double joystickY(){
-        return this.joystick.getY(); //return the value of the y-axis of the joystick controller
+
+    public double joystickY() {
+        return deadzoneFilter(this.joystick.getY()); //return the value of the y-axis of the joystick controller
     }
-    
-    public double joystickZ(){
-        return this.joystick.getZ(); //return the value of the z-axis of the joystick controller
+
+    public double joystickZ() {
+        return deadzoneFilter(this.joystick.getZ()); //return the value of the z-axis of the joystick controller
     }
-    
-    
-    
-    
-    
-    
-    
+
+    private double deadzoneFilter(double joyStickValue) {
+        if (Math.abs(joyStickValue) <= DEADZONE) {
+            return 0;
+        }
+        return joyStickValue;
+
+    }
+
 }
