63fe26eebf385d11bcef3e0700bc01f45e5b030a
Programming <Programming@10.30.107.10>
2014-01-24 21:44:41 -0600

    Created the base platform with a 1 joystick drive system.

---
+++ src/edu/wpi/first/wpilibj/paradigm/DriverControls.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.paradigm;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ *
+ * @author Programming
+ */
+public class DriverControls {
+    
+    private Joystick joystick = new Joystick(1); //Created and initialized the joystick controller
+    private Joystick xBox = new Joystick(2); //Created and initialized the xbox controller
+    
+    
+    public double joystickX(){
+        return this.joystick.getX(); //return the value of the x-axis of the joystick controller
+    }
+    
+    public double joystickY(){
+        return this.joystick.getY(); //return the value of the y-axis of the joystick controller
+    }
+    
+    public double joystickZ(){
+        return this.joystick.getZ(); //return the value of the z-axis of the joystick controller
+    }
+    
+    
+    
+    
+    
+    
+    
+}
