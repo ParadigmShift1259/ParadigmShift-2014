63daace7b8251490b379de05a277547701a11d2a
Programming <Programming@10.30.107.10>
2014-01-25 11:44:04 -0600

    Created the base platform with a 1 joystick drive system.

--- src/edu/wpi/first/wpilibj/paradigm/AdventureRick.java
+++ src/edu/wpi/first/wpilibj/paradigm/AdventureRick.java
@@ -4,27 +4,30 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.paradigm;
-
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 /**
  * The VM is configured to automatically run this class, and to call the
- functions corresponding to each mode, as described in the IterativeAdventureRick
- documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * functions corresponding to each mode, as described in the
+ * IterativeAdventureRick documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the manifest
+ * file in the resource directory.
  */
 public class AdventureRick extends IterativeRobot {
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     DriveTrain drive;
+    DriverControls controls;
+
     public void robotInit() {
-        
+        controls = new DriverControls();
+        drive = new DriveTrain(controls);
+
     }
 
     /**
@@ -40,12 +43,12 @@ public class AdventureRick extends IterativeRobot {
     public void teleopPeriodic() {
         drive.setPower();
     }
-    
+
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    
+
     }
-    
+
 }
