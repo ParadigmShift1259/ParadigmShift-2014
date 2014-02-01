571337f7ba4a7ee94fad7539a124e4319d394af8
Matt <Matt@10.30.101.38>
2014-02-01 15:02:38 -0600

    New Class

--- src/edu/wpi/first/wpilibj/paradigm/Shooter.java
+++ src/edu/wpi/first/wpilibj/paradigm/Shooter.java
@@ -5,6 +5,7 @@
  */
 
 package edu.wpi.first.wpilibj.paradigm;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 
 /**
@@ -15,8 +16,26 @@ public class Shooter {
     
     DriverControls operatorInputs;
     
+    private final int PORT_5 = 5;
+    private Joystick xBox = new Joystick(2);
+    private Talon kickermotor = new Talon(PORT_5);
+    private boolean buttonPressed;
+    private final int A_BUTTON = 1;
+    private double motorSpeed = 1.0;
+    
     public Shooter(DriverControls _operatorInputs) {
         this.operatorInputs = _operatorInputs;
+        
+    }
+    
+    public void kick() {
+        buttonPressed = xBox.getRawButton(A_BUTTON);
+        if (buttonPressed = true) {
+            kickermotor.set(motorSpeed);
+        } else {
+            kickermotor.set(0);
+        }
+        
     }
     
    //need to figure out moveable parts on the shooting mechanism before adding buttons/functions 
