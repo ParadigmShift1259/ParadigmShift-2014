d02c5c5f1987eadb3b39f6c9ed560386d7a3949c
Administrator <Administrator@4A-116-PC06>
2014-02-04 20:12:40 -0600

    Allison is awesome

--- src/edu/wpi/first/wpilibj/paradigm/Picker.java
+++ src/edu/wpi/first/wpilibj/paradigm/Picker.java
@@ -6,6 +6,7 @@
  */
 
 package edu.wpi.first.wpilibj.paradigm;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Encoder;
 
@@ -16,13 +17,27 @@ import edu.wpi.first.wpilibj.Encoder;
 public class Picker {
     
     DriverControls operatorInputs;
-    int loadPos = 757; //change value later, position wile loading
-    int shootPos = 2136; //change value later, position while shooting/aiming
-    int autoPos = 2048; //change value later, position at the beginning of the auto/match
-    int currentPos; //the picker's current pos(ition)
+    private Joystick xBox = new Joystick(2);
+    private int loadPos = 757; //change value later, position while loading
+    private int shootPos = 2136; //change value later, position while shooting/aiming
+    private int autoPos = 2048; //change value later, position at the beginning of the auto/match
+    private int currentPos; //the picker's current pos(ition)
+    private int selfDestruct = 0;
+    private final int BUTTON_X = 3; //this is the x butt on the controller
+    private boolean buttonPressed = false;
+    private Talon jaguar = new Talon(8); //used in the SpinGrabber method...
     
     public Picker(DriverControls _operatorInputs) {
         this.operatorInputs = _operatorInputs;
+    }
+    
+    public void spinGrabber() {
+        buttonPressed = xBox.getRawButton(BUTTON_X);
+        if (buttonPressed == true) {
+            jaguar.set(0.2);
+        } else {
+            jaguar.set(0);
+        }
     }
     //need to figure out moveable parts on the picker in order to assign functions
     
